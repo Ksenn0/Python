9adfcc5b9323509ae5017ae0bb638a8bec973da4
Kauann Senna <ksdev.1337@gmail.com>
2025-05-03 13:08:51 -0300

    Update and rename Ex014.py to conversor_de_temperatura.py

--- conversor_de_temperatura.py
+++ conversor_de_temperatura.py
@@ -1,4 +1,4 @@
 #conversor de temperatura
 c = float(input("Temperatura em °C: "))
 f = (9 * c / 5) + 32
-print("{}°C convertidos para Farenheight são: {}°F".format(c, f))
+print("{}°C convertidos para Farenheit são: {}°F".format(c, f))
